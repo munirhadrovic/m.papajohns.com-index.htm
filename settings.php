2d194694d3492cfd9fed403ff6e4e61835c25a93
novicaa <novicamiketic@gmail.com>
2015-03-30 10:50:30 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa=" Golden Gate Shopping Center, 4885 Golden Gate Pkwy, Naples, FL 34116, Sjedinjene Američke Države"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Naples, FL 34116";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=12393527272;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
